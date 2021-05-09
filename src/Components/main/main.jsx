bc9da1853502ce015372b164e14ac6afab249849
dudn1933 <dudn1933@naver.com>
2021-05-09 19:25:32 +0900

    무한스크롤 만들다가 잠시빠진 내용이 있었어서 다시 커밋

--- src/Components/main/main.jsx
+++ src/Components/main/main.jsx
@@ -1,10 +1,13 @@
 import { storeData } from "../../json/demo_data";
 import StoreList from "./storeList/StoreList"
+import {useContext} from "react"
+import {PostsContext} from "../../App"
 
 const Main = () => {
+    const {on, detail} = useContext(PostsContext);
     return (
         <>
-            {storeData.map((props) => {
+            {!on&&!detail&&storeData.map((props) => {
                 return (
                 <StoreList key={props.id} {...props} />
                 )
